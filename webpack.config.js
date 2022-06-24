d356be070ac1df37cbaa75647d217e9b50ae9c84
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-24 12:40:10 -0600

    Added webpack, babel, and typescript configuration.

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "/client/src/index.tsx"),
+  output: {
+    path: path.join(__dirname, "/client/dist"),
+    filename: "bundle.js",
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(css)$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
+  }
+};
