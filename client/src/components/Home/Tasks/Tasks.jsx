59d262851015425d775a6e71f75035337f2f3bee
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-25 09:41:52 -0600

    Added media queries and successfully positioned side menu and tasks divs.

--- client/src/components/Home/Tasks/Tasks.jsx
+++ client/src/components/Home/Tasks/Tasks.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { Container } from 'react-bootstrap';
 
+import './Tasks.css';
+
 const Tasks = () => {
   return (
-    <div></div>
+    <div id="Tasks">
+      <h1>Tasks</h1>
+      <p>There's supposed to be a bunch of text here so I am currently just trying to fill up space and write as many words as I can possibly think of in order to fill up all this space, speaking of space, it's so vast isn't it? There's tons of cool pictures from the Hubble space telescope and I am even more excited for Webb telescope to start sending back all the craziest and most fascinating pictures from deep space. Well, I think this should probably be enough text by now, so bye!</p>
+    </div>
   );
 };
 
